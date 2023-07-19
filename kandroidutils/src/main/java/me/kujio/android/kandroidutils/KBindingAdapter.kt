0e2ccd22591028db5e1b7c607d132ae9025beae9
kujio <imkujio@qq.com>
2023-07-20 01:15:59 +0800

    新增LoadMoreRecycleAdapter

--- kandroidutils/src/main/java/me/kujio/android/kandroidutils/KBindingAdapter.kt
+++ kandroidutils/src/main/java/me/kujio/android/kandroidutils/KBindingAdapter.kt
@@ -3,14 +3,21 @@ package me.kujio.android.kandroidutils
 import android.app.Activity
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import me.kujio.android.kandroidutils.databinding.ItemLoadMoreBinding
 
 fun EditText.showKeyboard() {
     isFocusable = true
@@ -20,7 +27,6 @@ fun EditText.showKeyboard() {
     val imm = context.getSystemService(InputMethodManager::class.java) as InputMethodManager
     imm.showSoftInput(this, 0)
 }
-
 
 
 @BindingAdapter("simpleAdapter")
@@ -96,5 +102,115 @@ class SimpleRecyclerAdapter(
 
     override fun getItemCount(): Int {
         return count()
+    }
+}
+
+@BindingAdapter("loadMoreAdapter")
+fun setLoadMoreAdapter(
+    recyclerView: RecyclerView, loadMoreAdapter: LoadMoreRecyclerAdapter?
+) {
+    if (loadMoreAdapter == null) return
+    if (recyclerView.context !is AppCompatActivity) return
+    recyclerView.layoutManager = LinearLayoutManager(recyclerView.context).apply {
+        orientation = LinearLayoutManager.VERTICAL
+    }
+    recyclerView.adapter = loadMoreAdapter
+}
+
+class LoadMoreRecyclerAdapter(
+    private val resId: (pos: Int) -> Int,
+    private val count: () -> Int,
+    private val load: suspend () -> Unit,
+    private val more: () -> Boolean,
+    private val bindData: (adapter: LoadMoreRecyclerAdapter, binding: ViewDataBinding, pos: Int) -> Unit
+) : RecyclerView.Adapter<LoadMoreRecyclerAdapter.LoadMoreViewHolder>() {
+    lateinit var ctx: Context
+
+    class LoadMoreViewHolder(val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root)
+
+    private lateinit var loadMoreViewHolder: LoadMoreViewHolder
+
+    private var scope: CoroutineScope? = null
+
+    private var isLoading = false
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        recyclerView.context.let {
+            ctx = it
+            if (it is LifecycleOwner) scope = it.lifecycleScope
+        }
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val visibleItemCount = recyclerView.layoutManager?.childCount ?: 0
+                val totalItemCount = recyclerView.layoutManager?.itemCount ?: 0
+                val firstVisibleItemPosition =
+                    (recyclerView.layoutManager as? LinearLayoutManager)?.findFirstVisibleItemPosition() ?: 0
+                if (visibleItemCount + firstVisibleItemPosition >= totalItemCount && firstVisibleItemPosition >= 0) {
+                    loadMoreViewHolder.binding.let { binding ->
+                        binding as ItemLoadMoreBinding
+                        if (more()) loadMore()
+                        else binding.state = 2
+                    }
+                }
+            }
+        })
+    }
+
+    private fun loadMore() {
+        if (isLoading) return
+        isLoading = true
+        loadMoreViewHolder.binding.let { binding ->
+            binding as ItemLoadMoreBinding
+            binding.state = 1
+            scope?.launch {
+                try {
+                    load()
+                    isLoading = false
+                } catch (_: Throwable) {
+                    binding.state = 3
+                }
+            } ?: run {
+                isLoading = false
+                binding.state = 3
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LoadMoreViewHolder {
+        return LoadMoreViewHolder(
+            DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context), viewType, parent, false
+            )
+        ).also {
+            if (viewType == R.layout.item_load_more)
+                loadMoreViewHolder = it
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position == itemCount - 1) {
+            R.layout.item_load_more
+        } else {
+            resId(position)
+        }
+    }
+
+    override fun onBindViewHolder(holder: LoadMoreViewHolder, position: Int) {
+        if (holder.binding is ItemLoadMoreBinding) {
+            if (more()) loadMore()
+            else holder.binding.state = 2
+            holder.binding.load = View.OnClickListener {
+                loadMore()
+            }
+        } else {
+            bindData(this, holder.binding, position)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return count() + 1
     }
 }
---
+++ kandroidutils/src/main/java/me/kujio/android/kandroidutils/loading/KLoadingView.kt
@@ -0,0 +1,141 @@
+package me.kujio.android.kandroidutils.loading
+
+import android.animation.Animator
+import android.animation.ValueAnimator
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.PointF
+import android.util.AttributeSet
+import android.view.View
+import me.kujio.android.kandroidutils.R
+import me.kujio.android.kandroidutils.dp
+import me.kujio.android.kandroidutils.primaryColor
+
+
+abstract class Object(
+    protected val size: Float,
+    protected val center: PointF,
+    protected val color: Int,
+) {
+    private lateinit var animator: Animator
+
+    protected val paint = Paint().apply {
+        isAntiAlias = true
+        color = this@Object.color
+        alpha = 125
+    }
+
+    fun setAnimator(block: Object.() -> Animator) {
+        animator = block()
+        animator.start()
+    }
+
+    fun setAlpha(value: Int) {
+        paint.alpha = value
+    }
+
+    abstract fun draw(canvas: Canvas)
+
+    fun recycle() {
+        animator.cancel()
+    }
+}
+
+class Circle(size: Float, center: PointF, color: Int) : Object(size, center, color) {
+    private val radius = size / 10f
+
+    override fun draw(canvas: Canvas) {
+        canvas.drawCircle(center.x, radius, radius, paint)
+    }
+}
+
+class Line(size: Float, center: PointF, color: Int) : Object(size, center, color) {
+    private val lineWidth = size / 8.0f
+    private val start = PointF(center.x, center.y - size / 2f)
+    private val end = PointF(center.x, start.y + 2 * lineWidth)
+
+    init {
+        paint.strokeWidth = lineWidth
+    }
+
+    override fun draw(canvas: Canvas) {
+        canvas.drawLine(start.x, start.y, end.x, end.y, paint)
+    }
+
+}
+
+class KLoadingView : View {
+    private val defSize = 24.dp
+    private lateinit var objects: Array<Object>
+    private val count = 8
+    private val center: PointF get() = PointF(width / 2f, height / 2f)
+
+    var color = context.primaryColor
+    var type = Type.LINE
+
+    enum class Type {
+        LINE, CIRCLE
+    }
+
+    constructor(context: Context) : this(context, null, 0)
+
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        if (attrs == null) return
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.KLoadingView)
+        type = if (typedArray.getInt(R.styleable.KLoadingView_type, 1) == 1) Type.CIRCLE else Type.LINE
+        color = typedArray.getColor(R.styleable.KLoadingView_color, context.primaryColor)
+        typedArray.recycle()
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val measuredWidth = resolveSize(defSize, widthMeasureSpec)
+        val measuredHeight = resolveSize(defSize, heightMeasureSpec)
+        setMeasuredDimension(measuredWidth, measuredHeight)
+    }
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+        initializeObjects()
+    }
+
+    private fun initializeObjects() {
+        val size = width.coerceAtMost(height).toFloat()
+        objects = Array(count) {
+            val obj = if (type == Type.CIRCLE) Circle(size, center, color)
+            else Line(size, center, color)
+            obj.setAnimator {
+                ValueAnimator.ofInt(125, 255, 125).apply {
+                    repeatCount = ValueAnimator.INFINITE
+                    duration = 1000
+                    startDelay = (it * 120).toLong()
+                    addUpdateListener { animation ->
+                        obj.setAlpha(animation.animatedValue as Int)
+                        invalidate()
+                    }
+                }
+            }
+            obj
+        }
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        if (canvas == null) return
+        objects.forEachIndexed { i, obj ->
+            canvas.save()
+            canvas.rotate((360 / count * i).toFloat(), center.x, center.y)
+            obj.draw(canvas)
+            canvas.restore()
+        }
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        for (obj in objects) {
+            obj.recycle()
+        }
+    }
+
+}
